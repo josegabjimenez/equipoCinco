614deaca23e66da328429b59ac5bc875d187beec
Cesar-velasquez117 <velasquez.cesar@correounivalle.edu.co>
2023-12-07 17:49:04 -0500

    Modification to the getListInventory function to use Firestore

--- app/src/main/java/com/example/equipoCinco/repository/InventoryRepository.kt
+++ app/src/main/java/com/example/equipoCinco/repository/InventoryRepository.kt
@@ -5,6 +5,7 @@ import com.example.equipoCinco.model.Product
 import com.example.equipoCinco.webservice.ApiService
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.tasks.await
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
@@ -32,7 +33,24 @@ class InventoryRepository  @Inject constructor(
 
     suspend fun getListInventory():MutableList<Inventory>{
         return withContext(Dispatchers.IO){
-            inventoryDao.getListInventory()
+            try {
+                val snapshot = db.collection("inventory").get().await()
+                val inventoryList = mutableListOf<Inventory>()
+                for (document in snapshot.documents) {
+                    val id = document.getLong("id")?.toInt() ?: 0
+                    val name = document.getString("name") ?: ""
+                    val price = document.getLong("price")?.toInt() ?: 0
+                    val quantity = document.getLong("quantity")?.toInt() ?: 0
+
+                    val item = Inventory(id, name, price, quantity)
+                    inventoryList.add(item)
+                }
+
+                inventoryList
+            } catch (e: Exception) {
+                e.printStackTrace()
+                mutableListOf()
+            }
         }
     }
 
